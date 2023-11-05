279f39e782304b5d969284c1caad484d8d16891b
Fernando Fresquer <dev@fresquer.dev>
2023-11-05 20:45:53 +0100

    fix css and rollback user in view

--- src/app/components/TextBlock.js
+++ src/app/components/TextBlock.js
@@ -8,38 +8,32 @@ export default function TextBlock() {
     const scrollWritting = () => { window.scrollBy(0, 48); }
 
     useEffect(() => {
-        const observer = new IntersectionObserver(entries => {
-            if (entries[0].isIntersecting) {
-                var typewriter = new Typewriter(textBlockRef.current);
+        var typewriter = new Typewriter(textBlockRef.current, {
+            delay: 75,
+        });
 
-                typewriter
-                    .pauseFor(500)
-                    .typeString('in a world where you can be anything, be kind')
-                    .pauseFor(timePause)
-                    .typeString('<br><br>there is so much to live for')
-                    .pauseFor(timePause)
-                    .callFunction(() => scrollWritting())
-                    .typeString('<br><br>there are so many stories to hear')
-                    .pauseFor(timePause)
-                    .callFunction(() => scrollWritting())
-                    .typeString('<br><br>there are so much life to share and enjoy')
-                    .pauseFor(timePause)
-                    .callFunction(() => scrollWritting())
-                    .typeString('<br><br>don’t let them win, don’t let fear take over')
-                    .pauseFor(timePause)
-                    .callFunction(() => scrollWritting())
-                    .typeString('<br><br>there is no place for fear')
-                    .pauseFor(timePause)
-                    .callFunction(() => scrollWritting())
-                    .typeString('<br><br><span style="font-weight: 600">grkzk.</span>')
-                    .pauseFor(timePause)
-                    .start();
-            }
-        }, { threshold: 1 });
-
-        observer.observe(textBlockRef.current);
-
-        return () => observer.unobserve(textBlockRef.current);
+        typewriter
+            .pauseFor(2500)
+            .typeString('in a world where you can be anything, be kind')
+            .pauseFor(timePause)
+            .typeString('<br><br>there is so much to live for')
+            .pauseFor(timePause)
+            .callFunction(() => scrollWritting())
+            .typeString('<br><br>there are so many stories to hear')
+            .pauseFor(timePause)
+            .callFunction(() => scrollWritting())
+            .typeString('<br><br>there are so much life to share and enjoy')
+            .pauseFor(timePause)
+            .callFunction(() => scrollWritting())
+            .typeString('<br><br>don’t let them win, don’t let fear take over')
+            .pauseFor(timePause)
+            .callFunction(() => scrollWritting())
+            .typeString('<br><br>there is no place for fear')
+            .pauseFor(timePause)
+            .callFunction(() => scrollWritting())
+            .typeString('<br><br><span style="font-weight: 600">grkzk.</span>')
+            .pauseFor(timePause)
+            .start();
     }, []);
 
     return (
