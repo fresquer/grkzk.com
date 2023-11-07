10b30ff891d2507e5f5f901895fc6c2712a7930d
Fernando Fresquer <dev@fresquer.dev>
2023-11-07 19:13:37 +0100

    fix url images

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
     url: 'https://grkzk.com',
     images: [
       {
-        url: '/images/og-desc-grkzk.png',
+        url: 'https://grkzk-com.vercel.app/images/og-desc-grkzk.png',
       },
     ],
   },
